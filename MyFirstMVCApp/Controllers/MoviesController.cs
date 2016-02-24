92b7ded3377d0281b663794772a5cc9a48845e79
John Schwartz <SchwartzJohnG@Gmail.com>
2016-02-23 19:01:39 -0600

    Added files via upload

---
+++ MyFirstMVCApp/Controllers/MoviesController.cs
@@ -0,0 +1,93 @@
+﻿using MyFirstMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private static MovieRepository _repo = new MovieRepository();
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(_repo.List());
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(string id)
+        {
+            return View();
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            try
+            {
+                _repo.Add(movie);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(string id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(string id)
+        {
+            return View(_repo.Find(id));
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ActuallyDelete(string id)
+        {
+            try
+            {
+                _repo.Delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyFirstMVCApp/Models/MovieRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstMVCApp.Models {
+    public class MovieRepository {
+
+        private IList<Movie> _movies = new List<Movie>() {
+                new Movie() { Title = "Star Wars", Director = "Lucas" },
+                new Movie() { Title = "Alien", Director = "Scott" },
+                new Movie() { Title = "Jurassic World", Director = "Spielberg" }
+            };
+
+        public IEnumerable<Movie> List() {
+            return _movies;
+        }
+
+        public void Add(Movie movie) {
+            _movies.Add(movie);
+        }
+
+        public Movie Find(string title) {
+
+            Movie movie = null;
+            foreach (var m in _movies) {
+                if (m.Title == title) {
+                    movie = m;
+                    break;
+                }
+            }
+            return movie;
+        }
+
+        public void Delete(string title) {
+
+            _movies.Remove(Find(title));
+        }
+    }
+}
